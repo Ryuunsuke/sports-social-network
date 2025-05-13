d1336c4c8a791ed44e951e1ab9ebd9547f730f18
Hoang Minh Quan Nguyen <quandien2002@gmail.com>
2025-05-13 16:28:42 +0200

    Final step of prototyping

--- static/js/utilities.js
+++ static/js/utilities.js
@@ -10,14 +10,17 @@ window.onclick = function(event) {
 function closemodals(){
     document.getElementById('id01').style.display='none';
     document.getElementById('id02').style.display='none';
+    document.body.classList.remove('modal-open');
 }
 
 function showRegister() {
     document.getElementById('id01').style.display = 'none'; // Hide login modal
     document.getElementById('id02').style.display = 'block'; // Show register modal
+    document.body.classList.add('modal-open');
 }
 
 function showLogin() {
     document.getElementById('id02').style.display = 'none'; // Hide register modal
     document.getElementById('id01').style.display = 'block'; // Show login modal
+    document.body.classList.add('modal-open');
 }
